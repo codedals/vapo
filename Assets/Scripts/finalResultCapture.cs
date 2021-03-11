07fbc67bab2eb111fddac5c3f723aed22583a274
Mitchell Carroll <mitch@mitchcarroll.com>
2021-03-11 15:31:15 +0900

    edit

---
+++ Assets/Scripts/finalResultCapture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleCloudStreamingSpeechToText;
+using UnityEngine.Video;
+
+public class finalResultCapture : MonoBehaviour
+{
+    private GameObject im;
+    private StreamingRecognizer streamer;
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        im = GameObject.Find("InputManager");
+        videoPlayer = GameObject.Find("VideoPlayer").GetComponent <VideoPlayer  > (); 
+        StreamingRecognizer streamer = im.GetComponent <StreamingRecognizer> ();
+        streamer.onFinalResult.AddListener(GetTranscript);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void GetTranscript(string results){
+        Debug.Log("something happened: " + results);
+        if(results.Contains("stop player")){
+            videoPlayer.Pause();
+        }
+        else if(results.Contains("start player")){
+            videoPlayer.Play();
+        }   
+    }
+}
---
+++ Assets/textUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textCapture : MonoBehaviour
+{
+    public string finalOutput;
+    // Start is called before the first frame update
+    void Start()
+    {                  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("MANE: " + finalOutput);
+    }   
+
+}
